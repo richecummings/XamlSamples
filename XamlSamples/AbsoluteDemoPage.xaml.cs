24ca131b6a5589488aaee6868cb0ab5b5ec1c48f
Richard Cummings <richardecummings@gmail.com>
2019-02-10 12:12:17 -0500

    Essential XAML syntax

---
+++ XamlSamples/AbsoluteDemoPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamlSamples
+{
+    public partial class AbsoluteDemoPage : ContentPage
+    {
+        public AbsoluteDemoPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
